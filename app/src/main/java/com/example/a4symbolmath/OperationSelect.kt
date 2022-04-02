539cec1efd400def2a1476d8d65caa48f96a96bd
SabeetHossain <shossain0186@bths.edu>
2022-04-01 20:35:45 -0400

    adding a neccessary file and editing loginactivity for some testing

---
+++ app/src/main/java/com/example/a4symbolmath/OperationSelect.kt
@@ -0,0 +1,11 @@
+package com.example.a4symbolmath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OperationSelect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choose_operation)
+    }
+}
